33c07a66e5d2d00fe330b5d9b5ad2f3749b6b103
DebordCompany <debord.info@gmail.com>
2022-06-27 15:30:30 +0200

    gran parte de la version mobile

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,13 @@
 var header_nav = document.querySelector("#header_nav");
 var list = document.querySelector("#list");
+var salida_menu = document.querySelector("#salida_menu");
 
 list.addEventListener("click", desplegar);
 function desplegar() {
   header_nav.classList.toggle("desplegar");
+}
+salida_menu.addEventListener("click", desplegar);
+
+function remove() {
+  header_nav.classList.remove("desplegar");
 }
